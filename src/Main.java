74bb19cf6527876cf25d8a3aa6a8252ac6964220
datquocngo <shaco14091999@gmail.com>
2020-02-11 17:44:05 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for (int i=201;i<=361;i++) {
+            System.out.println("case " + i + ":");
+            System.out.println("    break;");
+        }
+    }
+}
